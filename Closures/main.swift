1d44c8cc99cd55a42de7554cf1e28427e4f86ab3
Kamalpreet <kamal777682>
2020-02-14 20:27:11 -0500

    Initial Commit

---
+++ Closures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Closures
+//
+//  Created by MacStudent on 2020-02-14.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
